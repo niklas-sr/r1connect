ee4e377a283c36fbe66e5b903c10b6ca6d833ac1
niklas-sr <niklas.steinruecken@gmail.com>
2025-04-30 12:51:24 +0000

    moved enviroment variables to .env and changed storage to r1connect

--- docker/compose-entrypoint.sh
+++ docker/compose-entrypoint.sh
@@ -2,23 +2,23 @@
 
 set -e
 
-if [ ! -f /automatisch/storage/.env ]; then
+if [ ! -f /r1connect/storage/.env ]; then
   >&2 echo "Saving environment variables"
   ENCRYPTION_KEY="${ENCRYPTION_KEY:-$(openssl rand -base64 36)}"
   WEBHOOK_SECRET_KEY="${WEBHOOK_SECRET_KEY:-$(openssl rand -base64 36)}"
   APP_SECRET_KEY="${APP_SECRET_KEY:-$(openssl rand -base64 36)}"
-  echo "ENCRYPTION_KEY=$ENCRYPTION_KEY" >> /automatisch/storage/.env
-  echo "WEBHOOK_SECRET_KEY=$WEBHOOK_SECRET_KEY" >> /automatisch/storage/.env
-  echo "APP_SECRET_KEY=$APP_SECRET_KEY" >> /automatisch/storage/.env
+  echo "ENCRYPTION_KEY=$ENCRYPTION_KEY" >> /r1connect/storage/.env
+  echo "WEBHOOK_SECRET_KEY=$WEBHOOK_SECRET_KEY" >> /r1connect/storage/.env
+  echo "APP_SECRET_KEY=$APP_SECRET_KEY" >> /r1connect/storage/.env
 fi
 
-# initiate env. vars. from /automatisch/storage/.env file
-export $(grep -v '^#' /automatisch/storage/.env | xargs)
+# initiate env. vars. from /r1connect/storage/.env file
+export $(grep -v '^#' /r1connect/storage/.env | xargs)
 
 # migration for webhook secret key, will be removed in the future.
 if [[ -z "${WEBHOOK_SECRET_KEY}" ]]; then
   WEBHOOK_SECRET_KEY="$(openssl rand -base64 36)"
-  echo "WEBHOOK_SECRET_KEY=$WEBHOOK_SECRET_KEY" >> /automatisch/storage/.env
+  echo "WEBHOOK_SECRET_KEY=$WEBHOOK_SECRET_KEY" >> /r1connect/storage/.env
 fi
 
 echo "Environment variables have been set!"
